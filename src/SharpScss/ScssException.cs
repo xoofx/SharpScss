dfa0573fe859ad715fa49f6cce8c77f4a2e81d58
Alexandre Mutel <alexandre_mutel@live.com>
2024-11-01 18:34:56 +0100

    Update repo to net8.0

--- src/SharpScss/ScssException.cs
+++ src/SharpScss/ScssException.cs
@@ -1,50 +1,49 @@
-﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// Copyright (c) Alexandre Mutel. All rights reserved.
 // This file is licensed under the BSD-Clause 2 license. 
 // See the license.txt file in the project root for more information.
 
 using System;
 
-namespace SharpScss
+namespace SharpScss;
+
+/// <summary>
+/// Exception used by <see cref="Scss"/> to report errors from libsass.
+/// </summary>
+public class ScssException : Exception
 {
     /// <summary>
-    /// Exception used by <see cref="Scss"/> to report errors from libsass.
+    /// Initializes a new instance of the <see cref="ScssException" /> class.
     /// </summary>
-    public class ScssException : Exception
+    /// <param name="line">The line.</param>
+    /// <param name="column">The column.</param>
+    /// <param name="file">The file.</param>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="errorText">The error text.</param>
+    public ScssException(int line, int column, string file, string message, string errorText) : base(message)
     {
-        /// <summary>
-        /// Initializes a new instance of the <see cref="ScssException" /> class.
-        /// </summary>
-        /// <param name="line">The line.</param>
-        /// <param name="column">The column.</param>
-        /// <param name="file">The file.</param>
-        /// <param name="message">The message that describes the error.</param>
-        /// <param name="errorText">The error text.</param>
-        public ScssException(int line, int column, string file, string message, string errorText) : base(message)
-        {
-            Line = line;
-            Column = column;
-            File = file;
-            ErrorText = errorText;
-        }
-
-        /// <summary>
-        /// Gets the line the exception occured.
-        /// </summary>
-        public int Line { get; }
-
-        /// <summary>
-        /// Gets the column the exception occured.
-        /// </summary>
-        public int Column { get; }
-
-        /// <summary>
-        /// Gets the file the exception occured.
-        /// </summary>
-        public string File { get; }
-
-        /// <summary>
-        /// Gets a short error message describing the error. For a full message use the <see cref="Exception.Message"/> property.
-        /// </summary>
-        public string ErrorText { get; set; }
+        Line = line;
+        Column = column;
+        File = file;
+        ErrorText = errorText;
     }
+
+    /// <summary>
+    /// Gets the line the exception occured.
+    /// </summary>
+    public int Line { get; }
+
+    /// <summary>
+    /// Gets the column the exception occured.
+    /// </summary>
+    public int Column { get; }
+
+    /// <summary>
+    /// Gets the file the exception occured.
+    /// </summary>
+    public string File { get; }
+
+    /// <summary>
+    /// Gets a short error message describing the error. For a full message use the <see cref="Exception.Message"/> property.
+    /// </summary>
+    public string ErrorText { get; set; }
 }
--- src/SharpScss/ScssOutputStyle.cs
+++ src/SharpScss/ScssOutputStyle.cs
@@ -1,41 +1,40 @@
-﻿// Copyright (c) Alexandre Mutel. All rights reserved.
+// Copyright (c) Alexandre Mutel. All rights reserved.
 // This file is licensed under the BSD-Clause 2 license. 
 // See the license.txt file in the project root for more information.
-namespace SharpScss
+namespace SharpScss;
+
+/// <summary>
+/// Determines the output format of the final CSS style used by <see cref="Scss.ConvertToCss"/> and <see cref="Scss.ConvertFileToCss"/>.
+/// </summary>
+public enum ScssOutputStyle
 {
     /// <summary>
-    /// Determines the output format of the final CSS style used by <see cref="Scss.ConvertToCss"/> and <see cref="Scss.ConvertFileToCss"/>.
+    /// Nested format.
     /// </summary>
-    public enum ScssOutputStyle
-    {
-        /// <summary>
-        /// Nested format.
-        /// </summary>
-        Nested = 0,
+    Nested = 0,
 
-        /// <summary>
-        /// Expanded format.
-        /// </summary>
-        Expanded = 1,
+    /// <summary>
+    /// Expanded format.
+    /// </summary>
+    Expanded = 1,
 
-        /// <summary>
-        /// Compact format.
-        /// </summary>
-        Compact = 2,
+    /// <summary>
+    /// Compact format.
+    /// </summary>
+    Compact = 2,
 
-        /// <summary>
-        /// Compressed format.
-        /// </summary>
-        Compressed = 3,
+    /// <summary>
+    /// Compressed format.
+    /// </summary>
+    Compressed = 3,
 
-        /// <summary>
-        /// TODO: No documentation.
-        /// </summary>
-        Inspect = 4,
+    /// <summary>
+    /// TODO: No documentation.
+    /// </summary>
+    Inspect = 4,
 
-        /// <summary>
-        /// TODO: No documentation.
-        /// </summary>
-        Sass = 5
-    }
+    /// <summary>
+    /// TODO: No documentation.
+    /// </summary>
+    Sass = 5
 }
